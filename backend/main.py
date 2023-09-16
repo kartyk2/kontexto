451cb01e8aeb45edf538778835cd6c6b65a62277
Kartik Kumar <kartik.official.acc@gmail.com>
2023-09-16 16:54:38 +0530

    feat: Added some basic routes for matching the guess and getting related words for a word.

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import gensim
+from typing import List
 
 app = FastAPI()
+
+my_model = gensim.models.Word2Vec.load('word2vec-amazon-cell-accessories-reviews-short.model')
 
 @app.get("/")
 async def root():
     return ("Hello Welcome to knotexto")
 
+@app.get("/similar", response_model=List[str])
+async def get_similar(word: str):
+    if word in my_model.wv:
+        res = my_model.wv.most_similar(word, topn=10)
+        similar_words = [item[0] for item in res]
+        return similar_words
+    else:
+        raise HTTPException(status_code= 404, detail="i don't know this word")
 
+
+
+@app.post("/guess")
+async def match_guess(word: str):
+    answer = my_model.wv.similarity(word, "hell")
+    return answer
 
 
