8a4e2f21c15a1885a89218afd4e934ffc5ed1ec3
Kartik Kumar <kartik.official.acc@gmail.com>
2023-10-02 21:42:02 +0530

    minor improvements

--- backend/main.py
+++ backend/main.py
@@ -1,34 +1,12 @@
-from fastapi import FastAPI, HTTPException
-import gensim
-from typing import List
+from fastapi import FastAPI
+from backend.router.game_api import api_router
 
 
 app = FastAPI()
 
-my_model = gensim.models.Word2Vec.load('word2vec-amazon-cell-accessories-reviews-short.model')
+app.include_router(api_router)
 
 @app.get("/")
 async def root():
-    return ("Hello Welcome to knotexto")
-
-@app.get("/similar", response_model=List[str])
-async def get_similar(word: str):
-    if word in my_model.wv:
-        res = my_model.wv.most_similar(word, topn=10)
-        similar_words = [item[0] for item in res]
-        return similar_words
-    else:
-        raise HTTPException(status_code= 404, detail="i don't know this word")
-
-
-@app.post("/guess")
-async def match_guess(word: str):
-    try:
-        if word in my_model.wv:
-            res = my_model.wv.similarity(w1= word, w2 = "hell")
-            return {"similarity": float(res)}
-        else:
-            raise HTTPException(status_code= 404, detail="i don't know this word")
-    except Exception as error:
-        raise error
+    return ("Hello, Welcome to kontexto")
 
---
+++ backend/router/game_api.py
@@ -0,0 +1,39 @@
+from fastapi import APIRouter, HTTPException
+import gensim
+from typing import List
+import traceback
+import sys
+
+api_router = APIRouter()
+
+my_model = gensim.models.Word2Vec.load('word2vec-amazon-cell-accessories-reviews-short.model')
+
+
+@api_router.get("/similar", response_model=List[str])
+async def get_similar(word: str):
+    try:
+        if word in my_model.wv:
+            res = my_model.wv.most_similar(word, topn=10)
+            similar_words = [item[0] for item in res]
+            return similar_words
+        else:
+            raise HTTPException(status_code= 404, detail="i don't know this word")
+
+    except Exception as error:
+        raise HTTPException(500, detail= traceback.format_exception_only(error))
+    
+
+@api_router.post("/guess")
+async def match_guess(word: str):
+    try:
+        raise HTTPException(status_code= 405, detail= "HEHE")
+        z = 10 / 0
+        if word in my_model.wv:
+            res = my_model.wv.similarity(w1= word, w2 = "hell")
+            return {"similarity": float(res)}
+        else:
+            raise HTTPException(status_code= 404, detail="i don't know this word")
+    except Exception as error:
+        raise error.with_traceback()
+
+
